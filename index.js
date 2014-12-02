cb265a9e5837233f5bdff49b4bc164c80bf0f8aa
Tom MacWright <tom@macwright.org>
2014-12-02 10:11:24 -0500

    Rename internally

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var createPattern = function(path) {
   return {pattern: path, included: true, served: true, watched: false};
 };
 
-var initTape = function(files, tapeConfig) {
+var initTAP = function(files, tapConfig) {
     files.unshift(createPattern(__dirname + '/src/adapter.js'));
     files.unshift(createPattern(__dirname + '/src/tap-parser.js'));
 };
 
-initTape.$inject = ['config.files'];
+initTAP.$inject = ['config.files'];
 
 module.exports = {
-  'framework:tape': ['factory', initTape]
+  'framework:tap': ['factory', initTAP]
 };
