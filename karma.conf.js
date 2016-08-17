f719db94f011b1c06c1551abeec1777eb33910e5
bySabi Files <flxinformatico@gmail.com>
2016-08-17 11:58:35 +0200

    add travis support

--- karma.conf.js
+++ karma.conf.js
@@ -65,6 +65,10 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers:
     // https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['jsdom']
+    browsers: ['jsdom'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: process.env.CONTINUOUS_INTEGRATION === 'true'
   });
 };
